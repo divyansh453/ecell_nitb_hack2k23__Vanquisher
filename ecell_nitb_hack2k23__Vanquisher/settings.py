010c3e7e1910835d9b5ce968c42fad44c0f95904
divyansh453 <104566386+divyansh453@users.noreply.github.com>
2023-03-18 13:21:16 +0530

    Update settings.py

--- ecell_nitb_hack2k23__Vanquisher/settings.py
+++ ecell_nitb_hack2k23__Vanquisher/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecellnitbhack2k23vanquisher-production.up.railway.app','localhost']
+ALLOWED_HOSTS = ['ecellnitbhack2k23vanquisher-production.up.railway.app','ecellnitbhack2k23vanquisher-production-0ed0.up.railway.app','localhost']
 
 
 
