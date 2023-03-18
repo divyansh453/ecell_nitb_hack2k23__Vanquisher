c1911b898f6a0a4f978d5bfcde2920f0402b9d45
divyansh453 <104566386+divyansh453@users.noreply.github.com>
2023-03-18 13:32:42 +0530

    Update settings.py

--- ecell_nitb_hack2k23__Vanquisher/settings.py
+++ ecell_nitb_hack2k23__Vanquisher/settings.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from pathlib import Path
-from datetime import timedeltab=1
+from datetime import timedelta
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
