ec80f4e339ffa65a708696d580d16a3fb5793e15
Deepika5656tomar <104566303+Deepika5656tomar@users.noreply.github.com>
2023-05-07 23:49:27 +0530

    Update views.py

--- info_student/views.py
+++ info_student/views.py
@@ -16,6 +16,7 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy 
 import json 
 from account.models import User
+b=1
 from .utils import Util,Utill,Util1
 class StudentView(ListCreateAPIView):
     serializer_class=StudentSerializer
