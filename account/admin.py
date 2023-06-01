843a6ad61074d66d7160c27efc90b63e031f8bf4
Shipra Tripathi <104627391+tripathishipra@users.noreply.github.com>
2023-06-01 23:34:43 +0530

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from account.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
-
+v = 1
 
 class UserModelAdmin(BaseUserAdmin):
  
-
+    
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserModelAdmin
     # that reference specific fields on auth.User.
